2e71fb71a1825577c1ea019fe231a958d3d0e362
TheFakeWater <thefakewater@protonmail.com>
2020-06-07 21:19:52 +0200

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+from time import sleep
+import secrets
+from instabot import Bot
+
+bot = Bot()
+bot.login(secrets.Credentials.username, secrets.Credentials.password)  # Make a secret.py file and make a class
+# Credentials then make two strings: username and password
+
+
+hashtags = ["starwars", "starwarsfan", "anakinskywalker", "prequels", "originals"]  # Edit your hashtags
+while True:
+    for hashtag in hashtags:
+        users = bot.get_hashtag_users(hashtag)
+        bot.follow_users(users)
+        bot.like_hashtag(hashtag)
+        sleep(1)
