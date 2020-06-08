6ac9eeff78e67c9d8552e7c8eb4122304923b8a4
TheFakeWater <43514781+TheFakeWater@users.noreply.github.com>
2020-06-08 16:42:49 +0200

    changed the default login details

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
-
 from instabot import Bot
 
 bot = Bot()
-bot.login("yourusernamehere", "yourpasswordhere") #Don't forget to put your password and username !
-
+bot.login("yourusernamehere", "yourpasswordhere")  # Don't forget to put your password and username !
 
 hashtags = ["starwars", "starwarsfan", "anakinskywalker", "prequels", "originals"]  # Edit your hashtags
 while True:
-    #Follow people
+    # Follow people
     for hashtag in hashtags:
         users = bot.get_hashtag_users(hashtag)
         bot.follow_users(users)
         bot.like_hashtag(hashtag)
+        bot.max_following_to_followers_ratio(50)  # Ratio Following/Followers
