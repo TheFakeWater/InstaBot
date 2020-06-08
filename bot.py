718349d0f1f4693575b1c23eac96c2bd1f58dff9
TheFakeWater <43514781+TheFakeWater@users.noreply.github.com>
2020-06-08 16:27:59 +0200

    Add some comments

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ bot.login("yourusernamehere", "yourpasswordhere") #Don't forget to put your pass
 
 hashtags = ["starwars", "starwarsfan", "anakinskywalker", "prequels", "originals"]  # Edit your hashtags
 while True:
+    #Follow people
     for hashtag in hashtags:
         users = bot.get_hashtag_users(hashtag)
         bot.follow_users(users)
